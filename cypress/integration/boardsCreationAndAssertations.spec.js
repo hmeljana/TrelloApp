7592c690cc5d8ff7c375ab37a1be4a54699adf3f
Bojana <nikolovskabokica11@gmail.com>
2021-10-22 22:14:58 +0200

    add getters and strings to separate files

--- cypress/integration/boardsCreationAndAssertations.spec.js
+++ cypress/integration/boardsCreationAndAssertations.spec.js
@@ -1,22 +1,26 @@
+import myBoards from "../fixtures/myBoards.json";
+import boards from "../fixtures/boards.json";
+import qaBoard from "../fixtures/qaBoard.json";
+
 describe("Create Boards and Assert Data", () => {
 	it("Assertation of all boards", () => {
 		cy.intercept("GET", "/api/boards", { fixture: "boards.json" }).as("stubbedBoards");
 		cy.visit("/");
-		cy.get("div:nth-of-type(1) > h1.board_title").should("contain", "To Do");
-		cy.get("div:nth-of-type(2) > h1.board_title").should("contain", "In Progress");
-		cy.get("div:nth-of-type(3) > h1.board_title").should("contain", "In QA");
-		cy.get("div:nth-of-type(4) > h1.board_title").should("contain", "Done");
+		cy.get(myBoards.boardOneTitle).should("contain", boards[0].name);
+		cy.get(myBoards.boardTwoTitle).should("contain", boards[1].name);
+		cy.get(myBoards.boardThreeTitle).should("contain", boards[2].name);
+		cy.get(myBoards.boardFourTitle).should("contain", boards[3].name);
 		cy.get("@stubbedBoards")
 			.its("response")
 			.then((intercept) => {
 				//console.log(intercept);
-				expect(intercept.body[0].name).to.eq("To Do");
+				expect(intercept.body[0].name).to.eq(boards[0].name);
 				expect(intercept.body[0].starred).to.eq(false);
-				expect(intercept.body[1].name).to.eq("In Progress");
+				expect(intercept.body[1].name).to.eq(boards[1].name);
 				expect(intercept.body[1].starred).to.eq(true);
-				expect(intercept.body[2].name).to.eq("In QA");
+				expect(intercept.body[2].name).to.eq(boards[2].name);
 				expect(intercept.body[2].starred).to.eq(false);
-				expect(intercept.body[3].name).to.eq("Done");
+				expect(intercept.body[3].name).to.eq(boards[3].name);
 				expect(intercept.body[3].starred).to.eq(false);
 			});
 	});
@@ -28,13 +32,13 @@ describe("Create Boards and Assert Data", () => {
 			.its("response")
 			.then((intercept) => {
 				// console.log(intercept);
-				expect(intercept.body.name).to.eq("In QA");
-				expect(intercept.body.lists[0].title).to.eq("Tasks");
-				expect(intercept.body.tasks[0].title).to.eq("Stubbing network responses");
+				expect(intercept.body.name).to.eq(boards[2].name);
+				expect(intercept.body.lists[0].title).to.eq(qaBoard.lists[0].title);
+				expect(intercept.body.tasks[0].title).to.eq(qaBoard.tasks[0].title);
 				expect(intercept.body.tasks[0].completed).to.eq(false);
-				expect(intercept.body.tasks[1].title).to.eq("Changing parts of response data");
+				expect(intercept.body.tasks[1].title).to.eq(qaBoard.tasks[1].title);
 				expect(intercept.body.tasks[1].completed).to.eq(true);
-				expect(intercept.body.tasks[2].title).to.eq("Intercepting");
+				expect(intercept.body.tasks[2].title).to.eq(qaBoard.tasks[2].title);
 				expect(intercept.body.tasks[2].completed).to.eq(false);
 			});
 	});
